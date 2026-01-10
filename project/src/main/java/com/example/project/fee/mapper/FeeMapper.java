1f68943d8c5b86a91c149108bbff3074832be399
martin <tonybuoitoi2019@gmail.com>
2026-01-10 15:44:09 +0700

    Them truong id cho feeDTO

--- project/src/main/java/com/example/project/fee/mapper/FeeMapper.java
+++ project/src/main/java/com/example/project/fee/mapper/FeeMapper.java
@@ -1,4 +1,5 @@
 package com.example.project.fee.mapper;
+
 //import com.example.project.fee.dto.FeeCreateDTO;
 import com.example.project.fee.dto.FeeDTO;
 import com.example.project.fee.entity.Fee;
@@ -9,7 +10,7 @@ public class FeeMapper {
 
     public FeeDTO toDTO(Fee entity) {
         FeeDTO dto = new FeeDTO();
-        //dto.setId(entity.getId());
+        dto.setId(entity.getId());
         dto.setName(entity.getName());
         dto.setType(entity.getType());
         dto.setDefaultAmount(entity.getDefaultAmount());
@@ -27,13 +28,13 @@ public class FeeMapper {
         fee.setNote(dto.getNote());
         return fee;
     }
-    //thay bằng hàm mới chuyển từ FeeDTO sang Fee
+    // thay bằng hàm mới chuyển từ FeeDTO sang Fee
     // public Fee toEntity(Fee dto) {
-    //     Fee fee = new Fee();
-    //     fee.setName(dto.getName());
-    //     fee.setType(dto.getType());
-    //     fee.setDefaultAmount(dto.getDefaultAmount());
-    //     fee.setArea(dto.getArea());
-    //     return fee;
+    // Fee fee = new Fee();
+    // fee.setName(dto.getName());
+    // fee.setType(dto.getType());
+    // fee.setDefaultAmount(dto.getDefaultAmount());
+    // fee.setArea(dto.getArea());
+    // return fee;
     // }
 }
