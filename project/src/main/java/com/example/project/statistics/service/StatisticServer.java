aee95e28f9ce32d0780883af5e4665bb39c4a7c2
martin <tonybuoitoi2019@gmail.com>
2026-01-13 19:08:23 +0700

    Sửa lần cuối, hứa

--- project/src/main/java/com/example/project/statistics/service/StatisticServer.java
+++ project/src/main/java/com/example/project/statistics/service/StatisticServer.java
@@ -1,15 +1,16 @@
 package com.example.project.statistics.service;
 
-import com.example.project.statistics.dto.MonthlyRevenue.MonthlyRevenueInDTO;
 import com.example.project.statistics.dto.MonthlyRevenue.MonthlyRevenueOutDTO;
 import com.example.project.statistics.dto.FeeSummary.FeeSummaryOutDTO;
-import com.example.project.statistics.dto.FeeSummary.FeeSummaryInDTO;
-import com.example.project.statistics.dto.VoluntarySummary.VoluntarySummaryInDTO;
 import com.example.project.statistics.dto.VoluntarySummary.VoluntarySummaryOutDTO;
 import com.example.project.statistics.dto.ResidentAndHouseholdCountDTO;
+
 public interface StatisticServer {
-    FeeSummaryOutDTO statisticFeeSummary(FeeSummaryInDTO dto);
-    MonthlyRevenueOutDTO statisticMonthlyRevenue(MonthlyRevenueInDTO dto);
-    VoluntarySummaryOutDTO statisticVoluntary(VoluntarySummaryInDTO dto);
+    FeeSummaryOutDTO statisticFeeSummary(Long feeId, Integer year, Integer month);
+
+    MonthlyRevenueOutDTO statisticMonthlyRevenue(Integer year, Integer month);
+
+    VoluntarySummaryOutDTO statisticVoluntary(Integer year, Integer month);
+
     ResidentAndHouseholdCountDTO countResidents();
 }
